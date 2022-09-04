e68c4790635dae4abd1811dfcba6527a179ec6d2
tylere <tylere@google.com>
2022-09-04 14:56:34 -0700

    Minor change.

--- test_image_embed/_modidx.py
+++ test_image_embed/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://tylere.github.io',
                 'git_url': 'https://github.com/tylere/test-image-embed',
                 'lib_path': 'test_image_embed'},
-  'syms': {'test_image_embed.core': {'test_image_embed.core.foo': ('core.html#foo', 'test_image_embed/core.py')}}}
+  'syms': { 'test_image_embed.core': { 'test_image_embed.core.foo': ( 'https://tylere.github.io/core.html#foo',
+                                                                      'https://github.com/tylere/test-image-embed/blob/master/test_image_embed/core.py#L7')}}}
